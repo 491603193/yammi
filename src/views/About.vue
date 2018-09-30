3aac6054592930e92d9358b16a920dcea850ec92
yiyk <491603193@qq.com>
2018-09-30 13:34:54 +0800

    2018/09/30

---
+++ src/views/About.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="about">
+        <h1 @click="openToast">This is an about page</h1>
+    </div>
+</template>
+<script>
+    import { Toast } from 'mint-ui';
+    import { Indicator } from 'mint-ui';
+    export default {
+        name: 'About',
+        methods: {
+            openToast () {
+                // Toast('操作成功');
+                // Indicator.open();
+            }
+        }
+    }
+</script>
